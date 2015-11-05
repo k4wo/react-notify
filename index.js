b5b014c12159dce7a20d27c14cacf3217b93227f
k4wo <k4wo@outlook.com>
2015-11-05 22:55:03 +0100

    v2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 var React = require('react');
+
+
 var Notify = React.createClass({
+	displayName: "Notify",
 	key: 0,
 	getInitialState: function() {
 		return {};
@@ -15,7 +18,7 @@ var Notify = React.createClass({
 		this.addNotify(title, msg, time, 'info');
 	},
 	addNotify: function(title, msg, time, theme) {
-		var key = this.key++;
+		var key         = this.key++;
 		this.state[key] = { title: title, msg: msg, time: time, theme: theme };
 		this.setState(this.state);
 		this.countToHide(time, key);
@@ -31,32 +34,36 @@ var Notify = React.createClass({
 		this.setState(this.state);
 	},
 	render: function() {
-		var keys = Object.keys(this.state);
+		var keys  = Object.keys(this.state);
 		var state = this.state;
-		var hide = this.hideNotification;
-		var el = keys.map(function(key) {
-			return <Item id={key}
-										 key={key}
-										 theme={state[key].theme}
-										 hideNotification={hide}
-										 title={state[key].title}
-										 msg={state[key].msg}
-					/>
+		var hide  = this.hideNotification;
+		var el    = keys.map(function(key) {
+			return React.createElement(Item, {
+						id: key,
+						key: key,
+						theme: state[key].theme,
+						hideNotification: hide,
+						title: state[key].title,
+						msg: state[key].msg
+					}
+			)
 		});
-		return (<div className="notify-container">{el}</div>);
+		return (React.createElement("div", { className: "notify-container" }, el));
 	}
 });
+
 var Item = React.createClass({
+	displayName: "Item",
 	hideNotification: function() {
 		this.props.hideNotification(this.props.id);
 	},
 	render: function() {
 		return (
-				<div className={"notify-item "+this.props.theme} onClick={this.hideNotification}>
-					<p className="notify-title">{this.props.title}</p>
+				React.createElement("div", { className: "notify-item " + this.props.theme, onClick: this.hideNotification },
+						React.createElement("p", { className: "notify-title" }, this.props.title),
 
-					<p className="notify-body">{this.props.msg}</p>
-				</div>
+						React.createElement("p", { className: "notify-body" }, this.props.msg)
+				)
 		)
 	}
 });
