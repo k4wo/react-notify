4ca1b8f9a5e269e8e1c9235905252b12ca333c1f
k4wo <k4wo@outlook.com>
2017-05-19 12:12:26 +0200

    webpack prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,19 @@ const webpack = require('webpack');
 module.exports = {
   entry: {
     vendor: ['react'],
-    demo: './show.js',
+    demo: './demo/show.js',
     app: './index.js',
   },
   output: {
     path: __dirname,
-    filename: './[name].js',
+    filename: './demo/[name].js',
     publicPath: '/',
   },
   plugins: [
-    // new webpack.optimize.UglifyJsPlugin({
-    //   warnings: false,
-    //   conditionals: true,
-    //   unused: true,
-    //   comparisons: true,
-    //   sourceMap: false,
-    //   sequences: true,
-    //   dead_code: true,
-    //   evaluate: true,
-    //   if_return: true,
-    //   join_vars: true,
-    //   output: { comments: false },
-    // }),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.LoaderOptionsPlugin({ debug: false, minimize: true }),
     new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' }),
-    // new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
 
   ],
   module: {
